66c80ab053d244d0776dc36700fad88abf4e971e
Sérgio Santos <ssantos@alter-solutions.com>
2021-04-15 12:27:58 +0100

    Alterado para criar um projeto baseado em typescipt e npm

--- generateProjectReact.sh
+++ generateProjectReact.sh
@@ -16,20 +16,24 @@ timestamp=$(date +%s)
 nameProject1="new_project_"
 nameProject="${nameProject1}${timestamp}"
 
-echo -e " ${Red}Criar pasta do projeto"
-echo -e " ${Purple}Criar ficheiro de inicio de projeto"
-echo -e " ${Blue}Instalar dependencias${NC}"
+# echo -e " ${Red}Criar pasta do projeto"
+# echo -e " ${Purple}Criar ficheiro de inicio de projeto"
+# echo -e " ${Blue}Instalar dependencias${NC}"
 # mkdir "$nameProject" && 
-npx create-react-app "$nameProject" -y && 
+npx create-react-app "$nameProject" --use-npm --template typescript -y && 
 cd "$nameProject" && 
 npm init -y &&
 npm i -g eslint &&
 npm i -D git-commit-msg-linter &&
-npm i -D typescript @types/node &&
-npm i -D eslint-config-standard-with-typescript@11 eslint-plugin-import eslint-plugin-promise eslint-plugin-standard @typescript-eslint/eslint-plugin eslint-plugin-node --legacy-peer-deps &&
-# npm i -D eslint@6 eslint-config-standard-with-typescript@11 eslint-plugin-import eslint-plugin-promise eslint-plugin-standard @typescript-eslint/eslint-plugin eslint-plugin-node --legacy-peer-deps &&
+#   npm i -D typescript @types/node &&
+npm i -D eslint-config-standard-with-typescript eslint-plugin-import eslint-plugin-promise &&
+###### npm i -D eslint-plugin-standard ######## This package has been deprecated ###############
+npm i -D @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint-plugin-node &&
+#npm i -D eslint@6 eslint-config-standard-with-typescript@11 eslint-plugin-import eslint-plugin-promise eslint-plugin-standard @typescript-eslint/eslint-plugin eslint-plugin-node --legacy-peer-deps &&
 npm i -D lint-staged husky &&
-npm i -D jest @types/jest ts-jest --legacy-peer-deps
+npm i -D jest  &&
+npm i -D ts-jest @types/jest &&
+# npx ts-jest config:init
 #npm i -D jest@25 @types/jest ts-jest@25 --legacy-peer-deps
 
 
@@ -37,10 +41,14 @@ echo -e " ${Yellow}Trocar texto do ficheiro package.json"
 # echo -e " ${Red}este comando só esta preparado para Mac Os"
 # Trocar text do ficheiro
 filename="package.json"
-search='echo \\"Error: no test specified\\" && exit 1'
-# echo -e " ${search}"
-replace="jest"
+search="eject\": \"react-scripts eject"
+#search='echo \\"Error: no test specified\\" && exit 1'
+echo -e " ${search}"
+replace="lint:fix\": \"eslint '.\/src\/**\/*.{ts,tsx}' --fix"
+#replace="jest"
+#sed -i '' "s/$search/$replace/g" $filename
 sed -i '' "s/$search/$replace/g" $filename
+
 
 
 # mkdir "src"
@@ -52,46 +60,48 @@ echo ".vscode" >> .gitignore
 echo "coverage" >> .gitignore
 
 
-echo -e " ${Green}Criar ficheiro tsconfig.json"
-touch tsconfig.json
-echo '{' >> tsconfig.json
-echo '  "compilerOptions": {' >> tsconfig.json
-echo '      "target": "es5",' >> tsconfig.json
-echo '      "lib": [' >> tsconfig.json
-echo '          "dom",' >> tsconfig.json
-echo '          "dom.iterable",' >> tsconfig.json
-echo '          "esnext",' >> tsconfig.json
-echo '      ],' >> tsconfig.json
-echo '      "esModuleInterop": true,' >> tsconfig.json
-echo '      "module": "esnext",' >> tsconfig.json
-echo '      "moduleResolution": "node",' >> tsconfig.json
-echo '      "jsx": "react",' >> tsconfig.json
-echo '      "rootDir": "src",' >> tsconfig.json
-echo '      "baseUrl": "src",' >> tsconfig.json
-echo '      "allowJs": true,' >> tsconfig.json
-echo '      "skipLibCheck": true,' >> tsconfig.json
-echo '      "allowSyntheticDefaultImports": true,' >> tsconfig.json
-echo '      "strict": true,' >> tsconfig.json
-echo '      "forceConsistentCasingInFileNames": true,' >> tsconfig.json
-echo '      "resolveJsonModule": true,' >> tsconfig.json
-# echo '      "isolatedModules": true,' >> tsconfig.json
-echo '      "noEmit": true,' >> tsconfig.json
-echo '  },' >> tsconfig.json
-echo '  "include": [' >> tsconfig.json
-echo '      "src"' >> tsconfig.json
-echo '  ]' >> tsconfig.json
-echo '}' >> tsconfig.json
+# echo -e " ${Green}Criar ficheiro tsconfig.json"
+# touch tsconfig.json
+# echo '{' >> tsconfig.json
+# echo '  "compilerOptions": {' >> tsconfig.json
+# echo '      "target": "es5",' >> tsconfig.json
+# echo '      "lib": [' >> tsconfig.json
+# echo '          "dom",' >> tsconfig.json
+# echo '          "dom.iterable",' >> tsconfig.json
+# echo '          "esnext",' >> tsconfig.json
+# echo '      ],' >> tsconfig.json
+# echo '      "esModuleInterop": true,' >> tsconfig.json
+# echo '      "module": "esnext",' >> tsconfig.json
+# echo '      "moduleResolution": "node",' >> tsconfig.json
+# echo '      "jsx": "react",' >> tsconfig.json
+# echo '      "rootDir": "src",' >> tsconfig.json
+# echo '      "baseUrl": "src",' >> tsconfig.json
+# echo '      "allowJs": true,' >> tsconfig.json
+# echo '      "skipLibCheck": true,' >> tsconfig.json
+# echo '      "allowSyntheticDefaultImports": true,' >> tsconfig.json
+# echo '      "strict": true,' >> tsconfig.json
+# echo '      "forceConsistentCasingInFileNames": true,' >> tsconfig.json
+# echo '      "resolveJsonModule": true,' >> tsconfig.json
+# # echo '      "isolatedModules": true,' >> tsconfig.json
+# echo '      "noEmit": true,' >> tsconfig.json
+# echo '  },' >> tsconfig.json
+# echo '  "include": [' >> tsconfig.json
+# echo '      "src"' >> tsconfig.json
+# echo '  ]' >> tsconfig.json
+# echo '}' >> tsconfig.json
 
 
 touch .eslintrc.json
 echo '{' >> .eslintrc.json
 echo '  "extends": "standard-with-typescript",' >> .eslintrc.json
 echo '  "parserOptions": {' >> .eslintrc.json
-echo '      "project": ".tsconfig.json"' >> .eslintrc.json
+echo '      "project": "tsconfig.json"' >> .eslintrc.json
 echo '   },' >> .eslintrc.json
 echo '   "rules": {' >> .eslintrc.json
 echo '      "@typescript-eslint/consistent-type-definitions": "off",' >> .eslintrc.json
-echo '      "@typescript-eslint/strict-boolean-expressions": "off"' >> .eslintrc.json
+echo '      "@typescript-eslint/strict-boolean-expressions": "off",' >> .eslintrc.json
+echo '      "@typescript-eslint/explicit-member-accessibility": 0,' >> .eslintrc.json
+echo '      "@typescript-eslint/explicit-function-return-type": 0' >> .eslintrc.json
 echo '    }' >> .eslintrc.json
 echo '}' >> .eslintrc.json
 
@@ -100,6 +110,8 @@ touch .eslintignore
 echo "node_modules" >> .eslintignore
 echo ".vscode" >> .eslintignore
 echo "coverage" >> .eslintignore
+echo "src/reportWebVitals.ts" >> .eslintignore
+echo "src/react-app-env.d.ts" >> .eslintignore
 
 
 touch .lintstagedrc.json
@@ -130,4 +142,4 @@ echo "      '.+\\\\.ts$': 'ts-jest'" >> jest.config.js
 echo "  }" >> jest.config.js
 echo "}" >> jest.config.js
 
-eslint src/*.js --fix
+eslint './src/**/*.{ts,tsx}' --fix
